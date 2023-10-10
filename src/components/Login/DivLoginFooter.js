f02649d71b42a7c73c0a8e27be8eab5bed9db693
yoy2655 <dbstls87@gmail.com>
2023-10-10 16:51:36 +0900

    feat:로그인 페이지 구현

---
+++ src/components/Login/DivLoginFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css'
+
+const test = () => {
+    return (
+        <div className="DivLoginFooter">
+            <p>@ 2023 Onstagram from KOSA</p>
+        </div>
+    );
+};
+
+export default test;
