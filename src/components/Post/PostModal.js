64ce644b2d806a3dcfb10c96ef2569b0436f400b
Jinho Lee <sound4519@gmail.com>
2023-10-10 21:58:29 +0900

    feat:보관 스토리 페이지, 프로필 셋팅부분 모달창 구현, 검색 모달창, 메세지 구현중

--- src/components/Post/PostModal.js
+++ src/components/Post/PostModal.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { useState } from 'react'
-import { useRef } from 'react'
-import './Post.css'
+import React from "react"
+import { useState } from "react"
+import { useRef } from "react"
+import "./Post.css"
 
-import Post from '../../assets/Fictogram/Nav/post.png'
-import PostImg from '../../assets/Fictogram/Post/post-img.png'
-import Emoji from '../../assets/Fictogram/Post/emoji-gray.png'
-import Location from '../../assets/Fictogram/Post/location.png'
-import Arrow from '../../assets/Fictogram/Post/arrow.png'
-import Profile from '../../assets/Fictogram/Nav/profile.png'
+import Post from "../../assets/Fictogram/Nav/post.png"
+import PostImg from "../../assets/Fictogram/Post/post-img.png"
+import Emoji from "../../assets/Fictogram/Post/emoji-gray.png"
+import Location from "../../assets/Fictogram/Post/location.png"
+import Arrow from "../../assets/Fictogram/Post/arrow.png"
+import Profile from "../../assets/Fictogram/Nav/profile.png"
 
 function PostModal() {
   const [modal, setModal] = useState(false)
@@ -24,17 +24,17 @@ function PostModal() {
   }
 
   const togglePostModal = () => {
-    alert('게시가 완료되었습니다!')
+    alert("게시가 완료되었습니다!")
     setNextModal(!nextModal)
   }
 
-  const selectFile = useRef('')
+  const selectFile = useRef("")
 
   return (
     <div>
       <div onClick={toggleModal} className="btn-modal">
-        <img src={Post} />
-        <p>만들기</p>
+        <img src={Post} alt="만들기 이미지" />
+        <span>만들기</span>
       </div>
       {modal && (
         <div className="modal">
@@ -51,11 +51,11 @@ function PostModal() {
               </div>
               <div className="modal-body">
                 <div className="modal-imgSelect">
-                  <img src={PostImg} />
+                  <img src={PostImg} alt="사진&영상" />
                   <span>사진과 영상을 여기에 끌어다 놓으세요</span>
                   <input
                     type="file"
-                    style={{ display: 'none' }}
+                    style={{ display: "none" }}
                     ref={selectFile}
                   />
                   <button
@@ -85,12 +85,12 @@ function PostModal() {
               </div>
               <div className="post-body">
                 <div className="img-wrapper">
-                  <img src={PostImg} />
+                  <img src={PostImg} alt="첨부이미지" />
                 </div>
                 <div className="text-wrapper">
                   <div className="post-userInfo">
                     <div className="post-info">
-                      <img src={Profile} />
+                      <img src={Profile} alt="유저 프로필 이미지" />
                       <div>여기는 유저 정보</div>
                     </div>
                   </div>
@@ -100,7 +100,7 @@ function PostModal() {
                   <div className="post-etc">
                     <div>
                       <button type="button" className="emoji-btn">
-                        <img src={Emoji} />
+                        <img src={Emoji} alt="이모티콘" />
                       </button>
                     </div>
                     <div className="post-textLength">2200 / 2200</div>
@@ -110,15 +110,15 @@ function PostModal() {
                       type="text"
                       placeholder="위치 정보를 입력해주세요."
                     />
-                    <img src={Location} />
+                    <img src={Location} alt="지역 이미지" />
                   </div>
                   <div className="post-access">
                     <span>접근성</span>
-                    <img src={Arrow} />
+                    <img src={Arrow} alt="세부메뉴" />
                   </div>
                   <div className="post-advanced">
                     <span>고급설정</span>
-                    <img src={Arrow} />
+                    <img src={Arrow} alt="세부메뉴" />
                   </div>
                 </div>
               </div>
