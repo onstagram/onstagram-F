db9246ef09d001ee1db1eb4856e880ae88df1275
Jinho Lee <sound4519@gmail.com>
2023-10-14 12:01:39 +0900

    docs:netlify 배포를 위한 문서 수정3

--- src/components/Main/MainSection.js
+++ src/components/Main/MainSection.js
@@ -1,8 +1,20 @@
 import React from "react"
 import "./Main.css"
+import { useNavigate } from "react-router-dom"
 
 function MainSection() {
-  return <div className="mainSection">여기는 메인페이지야</div>
+  const navigate = useNavigate()
+
+  const goToLogin = () => {
+    navigate("/login")
+  }
+
+  return (
+    <div className="mainSection">
+      <span onClick={goToLogin}>로그인페이지로 가기</span>
+      여기는 메인페이지야
+    </div>
+  )
 }
 
 export default MainSection
