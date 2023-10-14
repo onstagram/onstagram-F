81554ef8ee12706d037370af24e13ce6101dc3ac
yoy2655 <dbstls87@gmail.com>
2023-10-14 11:00:35 +0900

    docs: 깃 머지중 문제발생 코드 수정

--- src/components/Post/PostModal.js
+++ src/components/Post/PostModal.js
@@ -28,15 +28,12 @@ function PostModal() {
     setNextModal(!nextModal)
   }
 
-<<<<<<< HEAD
+
   const fileClick = () => {
     selectFile.current.click()
   }
 
   const selectFile = useRef("")
-=======
-  const selectFile = useRef('')
->>>>>>> 0e3d70364e41ef73f8d7d438f19aafcf96df8f83
 
   return (
     <div>
